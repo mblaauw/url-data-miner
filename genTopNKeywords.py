c5a7093c3585c678e7a93929e609634598485680
mblaauw <michel.blaauw@gmail.com>
2015-11-18 20:42:42 +0100

    added term doc matrix functions

--- genTopNKeywords.py
+++ genTopNKeywords.py
@@ -1,10 +1,16 @@
 import os
+from langdetect import detect
 
 for i in os.listdir('raw_txt'):
     if i.endswith(".txt"):
 
-        print i
 
+        file = open('raw_txt/' + i, "r",)
+        lang = detect(file.read().encode('UTF-8'))
+        print lang
+
+
+        #vectorizer = text.CountVectorizer(input='raw_html/' + i, stop_words='english', min_df=1)
 
 
         continue
